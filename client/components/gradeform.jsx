2e7c92777d33cd9dfa8e915bcb9b3b6a451b9269
kevin,ej.lee@gmail.com <kevin.ej.lee@gmail.com>
2020-06-10 17:18:48 +0000

    wip

---
+++ client/components/gradeform.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class GradeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      course: '',
+      grade: ''
+    };
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Course:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Grade:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
