6c83a316a5f0fe1018afc73e52ef354b9a41c707
kevin,ej.lee@gmail.com <kevin.ej.lee@gmail.com>
2020-06-10 01:13:50 +0000

    user can see average grade completed

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -5,7 +5,19 @@ import Header from './header';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { grades: [] };
+    this.state = {
+      grades: []
+    };
+    this.getAverageGrade = this.getAverageGrade.bind(this);
+  }
+
+  getAverageGrade() {
+    let totalGrade = 0;
+    const gradeArr = this.state.grades;
+    for (let i = 0; i < gradeArr.length; i++) {
+      totalGrade += gradeArr[i].grade;
+    }
+    return totalGrade / gradeArr.length;
   }
 
   componentDidMount() {
@@ -21,7 +33,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header averageGrade={this.getAverageGrade()}/>
         <GradeTable grades={this.state.grades}/>
       </div>
     );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 export default class Header extends React.Component {
   render() {
-    return <h1>Student Grade Table</h1>;
+    return (
+      <>
+        <header className="row justify-content-between">
+          <h1 className="col-8">Student Grade Table</h1>
+          <div className="col-4">
+            Average Grade
+            <span className="badge badge-secondary">
+              {this.props.averageGrade}
+            </span>
+          </div>
+        </header>
+      </>
+    );
   }
 }
